b08d758def444d0fc7f8878d5722bde585b485ff
Rangel Acevedo <rangelfco123@gmail.com>
2021-01-27 01:54:08 -0400

    Se declararon variables

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ namespace PruebaGit
             Console.WriteLine("Prueba Con GitHub");
             Console.WriteLine("Que funcione");
             Console.WriteLine("Hola Mundo");
+
+            int num1;
+            int num2;
+            double resultado;
         }
     }
 }
