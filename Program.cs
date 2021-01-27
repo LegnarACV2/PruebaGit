5628e3c628329c72c6ce958edf89d4d2239a240e
Justin Jr <77954884+JustinLuuu@users.noreply.github.com>
2021-01-27 03:17:00 -0400

    Cambio de justin

--- Program.cs
+++ Program.cs
@@ -6,23 +6,10 @@ namespace PruebaGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Prueba Con GitHub");
-            Console.WriteLine("Que funcione");
-            Console.WriteLine("Hola Mundo");
-
-            int num1;
-            int num2;
-            double resultado;
-
-            Console.Write("Ingrese el primer numero");
-            num1 = int.Parse(Console.ReadLine());
-            Console.Write("Ingrese el segundo numero");
-            num2 = int.Parse(Console.ReadLine());
-
-            resultado = num1 + num2;
-
-            Console.WriteLine("La suma de los numeros es: " + resultado);
+            while (true)
+            {
+                Console.WriteLine("De aqui pa microsoft");
+            }
         }
     }
 }
